8b05d86d7e09a3053741ef96e57e2177965c62d2
Sev <82528808+iarum@users.noreply.github.com>
2023-03-11 14:36:22 +0400

    Fix

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-$(document).keypress(function (e) {
-    var key = e.which;
-    console.log(key);
-    if (key == 13)  // the enter key code
-    {
-        audio = new Audio('audio/kick.wav');
-        audio.play();
+$(document).ready(function () {
+    let stick = $('#stick');
+
+    let con = {
+        113: ["kick", 61, 52],
+        119: ["hit", 20, 89],
+        101: ["snare", 11, 66],
+        114: ["crash", 3, 29]
     }
-}); 
+
+    function play(sound, topPosition, leftPosition) {
+        let audio = new Audio(`audio/${sound}.mp3`);
+        audio.play();
+
+        // animate
+        stick.show().css({ top: topPosition + "%", left: leftPosition + "%", transform: 'rotate(-10deg)' });
+
+        setTimeout(function () {
+            stick.css('transform', 'rotate(0deg)');
+        }, 100)
+    }
+
+    $(document).on('keypress', function (e) {
+        if (typeof (con[e.which]) !== 'undefined') {
+            play(con[e.which][0], con[e.which][1], con[e.which][2]);
+        }
+    })
+})
